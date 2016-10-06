d07e2ba887838e0e5f6940dcc6ecf23819ca95e0
Victor Chavez <vrchavez07@gmail.com>
2016-10-06 07:02:59 -0700

    Create index.css

---
+++ index.css
@@ -0,0 +1,283 @@
+body {
+	height: auto;
+	width: 100%;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background-color: #2a3132;
+	overflow: visible;
+	overflow-x: hidden;
+}
+
+h1 {
+	font-family: 'Oswald', sans-serif;
+	font-size: 72px;
+	left: 0;
+	right: 0;
+	top: -40px;
+	position: absolute;
+	text-align: center;
+	color: #fcfdfe;
+}
+
+#calculator {
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 20%;
+	width: 650px;
+	height: 520px;
+	margin: auto;
+	text-align: center;
+	background-color: #8d230f;
+}
+
+#answers {
+	position: absolute;
+	top: 10px;
+	left: 10px;
+	right: 10px;
+	height: 150px;
+	background-color: #2a3132;
+}
+
+#output {
+	position: absolute;
+	right: 10px;
+	bottom: -30px;
+	font-family: 'Oswald', sans-serif;
+	font-size: 62px;
+	color: #fcfdfe;
+}
+
+#botl {
+	position: absolute;
+	left: 10px;
+	bottom: 10px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#botlth {
+	position: absolute;
+	left: 10px;
+	bottom: 80px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#botlse {
+	position: absolute;
+	left: 10px;
+	bottom: 150px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#botlfi {
+	position: absolute;
+	left: 10px;
+	bottom: 220px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#botlbfi {
+	position: absolute;
+	left: 10px;
+	bottom: 290px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#sec {
+	position: absolute;
+	left: 170px;
+	bottom: 10px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#secth {
+	position: absolute;
+	left: 170px;
+	bottom: 80px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#secse {
+	position: absolute;
+	left: 170px;
+	bottom: 150px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#secfi {
+	position: absolute;
+	left: 170px;
+	bottom: 220px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#secbfi {
+	position: absolute;
+	left: 170px;
+	bottom: 290px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#thi {
+	position: absolute;
+	left: 330px;
+	bottom: 10px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#thith {
+	position: absolute;
+	left: 330px;
+	bottom: 80px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#thise {
+	position: absolute;
+	left: 330px;
+	bottom: 150px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#thifi {
+	position: absolute;
+	left: 330px;
+	bottom: 220px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#thibfi {
+	position: absolute;
+	left: 330px;
+	bottom: 290px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#fou {
+	position: absolute;
+	left: 490px;
+	bottom: 10px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#fouth {
+	position: absolute;
+	left: 490px;
+	bottom: 80px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#fouse {
+	position: absolute;
+	left: 490px;
+	bottom: 150px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#foufi {
+	position: absolute;
+	left: 490px;
+	bottom: 220px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+#foubfi {
+	position: absolute;
+	left: 490px;
+	bottom: 290px;
+	height: 60px;
+	width: 150px;
+	background-color: #2a3132;
+}
+
+.num {
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 0;
+	width: 100%;
+	height: 60px;
+	text-align: center;
+	font-family: 'Oswald', sans-serif;
+	font-size: 40px;
+	color: #fcfdfe;
+	background-color: transparent;
+	outline: none;
+	border: none;
+	cursor: pointer;
+}
+
+.numMult {
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 10px;
+	width: 100%;
+	height: 60px;
+	text-align: center;
+	font-family: 'Oswald', sans-serif;
+	font-size: 40px;
+	color: #fcfdfe;
+	background-color: transparent;
+	outline: none;
+	border: none;
+	cursor: pointer;
+}
+
+.numDif {
+	position: absolute;
+	left: 0;
+	right: 0;
+	top: 0;
+	width: 100%;
+	height: 60px;
+	text-align: center;
+	font-family: 'Material Icons', sans-serif;
+	font-size: 40px;
+	color: #fcfdfe;
+	background-color: transparent;
+	outline: none;
+	border: none;
+	cursor: pointer;
+}
