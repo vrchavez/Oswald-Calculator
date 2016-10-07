43e8121e9bdcaaa8c3932244c762f495334f5f5d
Victor Chavez <vrchavez07@gmail.com>
2016-10-06 22:13:07 -0700

    Update Changer.js

--- Changer.js
+++ Changer.js
@@ -2,49 +2,142 @@ var topStr= "";
 var nu= 0;
 var oper= "";
 function changeTop(id_checked){
-	if(id_checked === "b"){
-		topStr += "1";
-		document.getElementById('output').innerHTML = topStr;
+	if(id_checked === "a"){
+		if((oper = "=") && (topStr === "")){
+			nu *= -1;
+			oper = "";
+			document.getElementById('output').innerHTML = nu;
+		}else if((topStr === "") && (nu === 0)){
+			document.getElementById('output').innerHTML = nu;
+		}else if((topStr != "") && (nu === 0)){
+			nu += Number(topStr);
+			nu *= -1;
+			document.getElementById('output').innerHTML = nu;
+		}
+		else{
+			nu *= -1;
+			document.getElementById('output').innerHTML = nu;
+		}
+	}else if(id_checked === "b"){
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += "1";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += "1";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "c"){
-		topStr += "4";
-		document.getElementById('output').innerHTML = topStr;
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += "4";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += "4";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "d"){
-		topStr += "7";
-		document.getElementById('output').innerHTML = topStr;
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += "7";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += "7";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "e"){
 		topStr = "";
 		nu = 0;
 		oper = "";
 		document.getElementById('output').innerHTML = "0";
 	}else if(id_checked === "f"){
-		topStr += "0";
-		document.getElementById('output').innerHTML = topStr;
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += "";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += "0";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "g"){
-		topStr += "2";
-		document.getElementById('output').innerHTML = topStr;
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += "2";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += "2";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "h"){
-		topStr += "5";
-		document.getElementById('output').innerHTML = topStr;
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += "5";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += "5";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "i"){
-		topStr += "8";
-		document.getElementById('output').innerHTML = topStr;
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += "8";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += "8";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "j"){
 		topStr = "";
 		nu = 0;
 		oper = "";
 		document.getElementById('output').innerHTML = "0";
 	}else if(id_checked === "k"){
-		topStr += ".";
-		document.getElementById('output').innerHTML = topStr;
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += ".";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += ".";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "l"){
-		topStr += "3";
-		document.getElementById('output').innerHTML = topStr;
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += "3";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += "3";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "m"){
-		topStr += "6";
-		document.getElementById('output').innerHTML = topStr;
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += "6";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += "6";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "n"){
-		topStr += "9";
-		document.getElementById('output').innerHTML = topStr;
+		if(oper === "="){
+			nu = 0;
+			oper = "";
+			topStr += "9";
+			document.getElementById('output').innerHTML = topStr;
+		}else{
+			topStr += "9";
+			document.getElementById('output').innerHTML = topStr;
+		}
 	}else if(id_checked === "o"){
 		if((topStr.length === 1) || (topStr.length === 0)){
 			topStr = "";
@@ -60,6 +153,9 @@ function changeTop(id_checked){
 			topStr = "";
 			document.getElementById('output').innerHTML = "+";
 		}else if((nu != 0) && (oper === "")){
+			oper = "+";
+			document.getElementById('output').innerHTML = "+";
+		}else if(oper === "="){
 			oper = "+";
 			document.getElementById('output').innerHTML = "+";
 		}else{
@@ -94,6 +190,9 @@ function changeTop(id_checked){
 		}else if((nu != 0) && (oper === "")){
 			oper = "-";
 			document.getElementById('output').innerHTML = "-";
+		}else if(oper === "="){
+			oper = "-";
+			document.getElementById('output').innerHTML = "-";
 		}else{
 			if(oper === "+"){
 				nu += Number(topStr);
@@ -124,6 +223,9 @@ function changeTop(id_checked){
 			topStr = "";
 			document.getElementById('output').innerHTML = "*";
 		}else if((nu != 0) && (oper === "")){
+			oper = "*";
+			document.getElementById('output').innerHTML = "*";
+		}else if(oper === "="){
 			oper = "*";
 			document.getElementById('output').innerHTML = "*";
 		}else{
@@ -158,6 +260,9 @@ function changeTop(id_checked){
 		}else if((nu != 0) && (oper === "")){
 			oper = "/";
 			document.getElementById('output').innerHTML = "/";
+		}else if(oper === "="){
+			oper = "/";
+			document.getElementById('output').innerHTML = "/";
 		}else{
 			if(oper === "+"){
 				nu += Number(topStr);
@@ -184,24 +289,48 @@ function changeTop(id_checked){
 	}else if(id_checked === "equ") {
 		if(oper === "+"){
 			nu += Number(topStr);
-			document.getElementById('output').innerHTML = nu;
-			oper = "";
-			topStr = "";
-		} else if(oper === "-"){					
+			if(nu.toString().length > 15){
+				document.getElementById('output').innerHTML = nu.toString().substr(0, 15);
+				oper = "=";
+				topStr = "";
+			} else{
+				document.getElementById('output').innerHTML = nu;
+				oper = "=";
+				topStr = "";
+			}
+		} else if(oper === "-"){
 			nu -= Number(topStr);
-			document.getElementById('output').innerHTML = nu;
-			oper = "";
-			topStr = "";					
+			if(nu.toString().length > 15){
+				document.getElementById('output').innerHTML = nu.toString().substr(0, 15);
+				oper = "=";
+				topStr = "";
+			} else{
+				document.getElementById('output').innerHTML = nu;
+				oper = "=";
+				topStr = "";
+			}
 		} else if(oper === "*"){			
 			nu *= Number(topStr);
-			document.getElementById('output').innerHTML = nu;
-			oper = "";
-			topStr = "";
+			if(nu.toString().length > 15){
+				document.getElementById('output').innerHTML = nu.toString().substr(0, 15);
+				oper = "=";
+				topStr = "";
+			} else{
+				document.getElementById('output').innerHTML = nu;
+				oper = "=";
+				topStr = "";
+			}
 		} else if(oper === "/") {
 			nu /= Number(topStr);
-			document.getElementById('output').innerHTML = nu;
-			oper = "";
-			topStr = "";
+			if(nu.toString().length > 15){
+				document.getElementById('output').innerHTML = nu.toString().substr(0, 15);
+				oper = "=";
+				topStr = "";
+			} else{
+				document.getElementById('output').innerHTML = nu;
+				oper = "=";
+				topStr = "";
+			}
 		}
 	}					
 }
